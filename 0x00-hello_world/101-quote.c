60ab925f2f172ea63cc02063d473f7196394ecc4
Apex-D-Great <apelogunemmanuel5@gmail.com>
2022-06-29 07:02:22 -0700

    fourth commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  *main - prints the size of various types
  *Return: 0 if sucess
  */
-int main()
+int main(void)
 {
 	write(2,"and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n",59);
 	return (1);
